514be3ada565b677521307c0dc5301f7e6c2b403
karoljanowski <karol.jj@icloud.com>
2024-09-03 23:56:02 +0200

    test v6

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -1,10 +1,14 @@
 import { getOrder } from "@/lib/actions";
 
-const Page = ({ params }: { params: { id: string } }) => {
-
+const Page = async ({ params }: { params: { id: string } }) => {
+    const order = await getOrder(Number(params.id))
+    if (!order) return <div>error</div>
     return (
         <div>
-            {params.id}
+            <div className="text-6xl">masz synek zamowienie numer {order?.id}</div>
+            <div className="text-6xl flex gap-2">zamowiles {order?.orderItems.map(item => {
+                return <div key={item.id}>{item.quantity} x {item.product.name}</div>
+            })}</div>
         </div>
     );
 }
--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,15 +1,13 @@
 import OrderList from "@/components/dashboard/OrderList";
 import { getOrders } from "@/lib/actions";
-export const dynamic = 'force-dynamic'
 const Page = async () => {
     const orders = await getOrders()
-    console.log(orders)
 
     return (
         <>
             <h2 className="text-3xl font-bold">Orders</h2>
             <p className="text-lg text-gray-600">Recent orders from your restaurant</p>
-            {/* <OrderList orders={orders} /> */}
+            <OrderList orders={orders} />
         </>
     );
 }
