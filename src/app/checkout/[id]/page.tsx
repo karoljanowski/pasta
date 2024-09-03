549ea3e272358ce94f9bb72e3f25d4d22b5028a2
karoljanowski <karol.jj@icloud.com>
2024-09-03 23:19:52 +0200

    test

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -1,14 +1,10 @@
 import { getOrder } from "@/lib/actions";
 
-const Page = async ({ params }: { params: { id: string } }) => {
-    const order = await getOrder(Number(params.id))
-    if (!order) return <div>error</div>
+const Page = ({ params }: { params: { id: string } }) => {
+
     return (
         <div>
-            <div className="text-6xl">masz synek zamowienie numer {order?.id}</div>
-            <div className="text-6xl flex gap-2">zamowiles {order?.orderItems.map(item => {
-                return <div key={item.id}>{item.quantity} x {item.product.name}</div>
-            })}</div>
+            {params.id}
         </div>
     );
 }
