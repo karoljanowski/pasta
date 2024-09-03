3e78cbaae132bfb01c91cc199bd4647132383e74
karoljanowski <karol.jj@icloud.com>
2024-09-03 23:44:25 +0200

    Update page.tsx

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -2,13 +2,13 @@ import OrderList from "@/components/dashboard/OrderList";
 import { getOrders } from "@/lib/actions";
 export const dynamic = 'force-dynamic'
 const Page = async () => {
-    const orders = await getOrders()
+    // const orders = await getOrders()
 
     return (
         <>
             <h2 className="text-3xl font-bold">Orders</h2>
             <p className="text-lg text-gray-600">Recent orders from your restaurant</p>
-            <OrderList orders={orders} />
+            {/* <OrderList orders={orders} /> */}
         </>
     );
 }
