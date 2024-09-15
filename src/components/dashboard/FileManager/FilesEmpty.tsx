50a853bc6112927b2133c144ae9e59f35b7b4932
karoljanowski <karol.jj@icloud.com>
2024-09-16 00:12:27 +0200

    fix texts

--- src/components/dashboard/FileManager/FilesEmpty.tsx
+++ src/components/dashboard/FileManager/FilesEmpty.tsx
@@ -1,7 +1,7 @@
 const FilesEmpty = () => {
     return (
         <div className="text-center mt-5">
-            <h2 className="text-3xl font-bold">You didn't upload images yet</h2>
+            <h2 className="text-3xl font-bold">You did not upload images yet</h2>
             <p className="text-lg text-gray-600">Please upload file to get list</p>
         </div>
     );
