a177e0e4aa0bfa90dde57feadaef224548bc454d
karoljanowski <karol.jj@icloud.com>
2024-09-25 23:29:30 +0200

    fix build error

--- src/components/dashboard/Settings/ChangePassword.tsx
+++ src/components/dashboard/Settings/ChangePassword.tsx
@@ -5,7 +5,7 @@ const ChangePassword = () => {
         <div className="pt-8 mt-8 border-t border-gray-200">
             <h3 className="text-xl flex gap-3 items-center"><User className="w-4 h-4" />Change password</h3>
             <span className="text-gray-500 text-sm">
-                Change your password here. Make sure it's at least 8 characters long.
+                Change your password here. Make sure it is at least 8 characters long.
             </span>
             <ChangePasswordForm />
         </div>
