1d8d95d09a69eef00328ad2c5c41a64cf9510fd9
karoljanowski <karol.jj@icloud.com>
2024-09-30 23:45:18 +0200

    fix Upload file

--- src/components/dashboard/FileManager/FileUpload.tsx
+++ src/components/dashboard/FileManager/FileUpload.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { addFile } from "@/lib/actions"
@@ -9,12 +9,10 @@ import { Image } from "lucide-react"
 import toast from 'react-hot-toast'
 import FormButton from '@/components/FormButton'
 
-
 const FileUpload = () => {
     const [state, action] = useFormState(addFile, { success: false })
     const [open, setOpen] = useState(false)
     const [fileName, setFileName] = useState("Choose file")
-    const formRef = useRef<HTMLFormElement>(null);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -27,9 +25,7 @@ const FileUpload = () => {
             setOpen(false)
             toast.success('File uploaded')
 
-            if (formRef.current) {
-                formRef.current.reset()
-            }
+            setFileName('Choose file')
         }
     }, [state])
 
